f952b9ff08508a39239d44783e10041aad47a0c6
Sasha Abramowitz <reallysasha@gmail.com>
2023-10-09 10:13:04 +0200

    nvim flake8 manually set default line lenght

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -322,8 +322,10 @@ null_ls.setup({
   sources = {
     null_ls.builtins.formatting.black,
     null_ls.builtins.formatting.isort,
-    null_ls.builtins.diagnostics.flake8,
     null_ls.builtins.diagnostics.mypy,
+    null_ls.builtins.diagnostics.flake8.with(
+      { extra_args = { "--max-line-length=100" } }
+    ),
   },
 })
 
