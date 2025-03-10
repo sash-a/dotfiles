ee7cba634e1a7e0546743d43de70433ad7dc8353
Sasha Abramowitz <reallysasha@gmail.com>
2025-03-10 08:48:36 +0200

    chore: default model for and markdown rendering for copilot

--- .config/nvim/lua/plugins/ai.lua
+++ .config/nvim/lua/plugins/ai.lua
@@ -1,23 +1,31 @@
 return {
-  "zbirenbaum/copilot.lua",
-  cmd = "Copilot",
-  build = ":Copilot auth",
-  event = "BufReadPost",
-  opts = {
-    suggestion = {
-      enabled = not vim.g.ai_cmp,
-      auto_trigger = true,
-      hide_during_completion = vim.g.ai_cmp,
-      keymap = {
-        accept = false, -- handled by nvim-cmp / blink.cmp
-        next = "<M-]>",
-        prev = "<M-[>",
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    build = ":Copilot auth",
+    event = "BufReadPost",
+    opts = {
+      suggestion = {
+        enabled = not vim.g.ai_cmp,
+        auto_trigger = true,
+        hide_during_completion = vim.g.ai_cmp,
+        keymap = {
+          accept = false, -- handled by nvim-cmp / blink.cmp
+          next = "<M-]>",
+          prev = "<M-[>",
+        },
+      },
+      panel = { enabled = false },
+      filetypes = {
+        markdown = true,
+        help = true,
       },
     },
-    panel = { enabled = false },
-    filetypes = {
-      markdown = true,
-      help = true,
+  },
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    opts = {
+      model = "claude-3.7-sonnet-thought",
     },
   },
 }
