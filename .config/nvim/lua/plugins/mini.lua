68a67cb85492f4ac7207f872314dc8de48f28c92
Sasha Abramowitz <reallysasha@gmail.com>
2025-08-31 18:10:29 +0200

    chore: clean up mini plugins

--- .config/nvim/lua/plugins/mini.lua
+++ .config/nvim/lua/plugins/mini.lua
@@ -1,12 +1,35 @@
-MiniDeps.add('nvim-mini/mini.bufremove')
-require('mini.bufremove').setup()
+-- UI
+MiniDeps.add('nvim-mini/mini.icons')
+require('mini.icons').setup()
 
--- mini plugins
+MiniDeps.add('nvim-mini/mini.notify')
+require('mini.notify').setup()
+
+MiniDeps.add('nvim-mini/mini.starter')
+require('mini.starter').setup()
+
+MiniDeps.add('nvim-mini/mini.extra')
+require('mini.extra').setup()
+
+MiniDeps.add('nvim-mini/mini.indentscope')
+require('mini.indentscope').setup({ symbol = "│" })
+vim.cmd('highlight MiniIndentscopeSymbol guifg=#504945') -- set colour of the indent to grey
+
+MiniDeps.add('nvim-mini/mini.animate')
+require('mini.animate').setup({
+    cursor = { enable = false, },
+    scroll = { enable = true, },
+})
+
+-- text manipulation
 MiniDeps.add('nvim-mini/mini.ai')
 require("mini.ai").setup()
 
 MiniDeps.add('nvim-mini/mini.pairs')
 require('mini.pairs').setup()
+
+MiniDeps.add('nvim-mini/mini.cursorword')
+require('mini.cursorword').setup()
 
 MiniDeps.add('nvim-mini/mini.surround')
 require('mini.surround').setup({
@@ -15,57 +38,26 @@ require('mini.surround').setup({
         delete = 'gsd',    -- Delete surrounding
         highlight = 'gsh', -- Highlight surrounding
         replace = 'gsr',   -- Replace surrounding
-
-        suffix_last = 'l', -- Suffix to search with "prev" method
-        suffix_next = 'n', -- Suffix to search with "next" method
     },
 })
 
-
-MiniDeps.add('nvim-mini/mini.extra')
-require('mini.extra').setup()
+-- navigation
+MiniDeps.add('nvim-mini/mini.bufremove')
+require('mini.bufremove').setup()
 
 MiniDeps.add('nvim-mini/mini.files')
 require('mini.files').setup({ mappings = { synchronize = "<CR>" } })
-
-MiniDeps.add('nvim-mini/mini.icons')
-require('mini.icons').setup()
-
-MiniDeps.add('nvim-mini/mini.cursorword')
-require('mini.cursorword').setup()
-
-MiniDeps.add('nvim-mini/mini.notify')
-require('mini.notify').setup()
-
-MiniDeps.add('nvim-mini/mini.starter')
-require('mini.starter').setup()
-
-MiniDeps.add('nvim-mini/mini.animate')
-require('mini.animate').setup({
-    -- Cursor path
-    cursor = { enable = false, },
-    scroll = { enable = true, },
-})
-
 
 MiniDeps.add('nvim-mini/mini.jump2d')
 require('mini.jump2d').setup({
     labels = "etovxqpdygfblzhckisuran",
     view = {
-        -- Whether to dim lines with at least one jump spot
-        dim = true,
-        n_steps_ahead = 1,
+        dim = true, -- Whether to dim lines with at least one jump spot
+        n_steps_ahead = 0,
     },
     allowed_windows = {
         current = true,
         not_current = false,
     },
-    mappings = {
-        start_jumping = '<leader>h',
-    },
+    mappings = { start_jumping = '<leader>h' },
 })
-
-
-MiniDeps.add('nvim-mini/mini.indentscope')
-require('mini.indentscope').setup({ symbol = "│" })
-vim.cmd('highlight MiniIndentscopeSymbol guifg=#504945') -- set colour of the indent to grey
